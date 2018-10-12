4c2d65d15f1eb1d368bc4e9eb0e91a2b4f7bcfec
Henry Tsai <henrytsai@outlook.com>
2018-10-12 11:23:19 -0700

    Tests - Boosted all tested classes to be above 80% code coverage.

---
+++ tests/Config.spec.ts
@@ -0,0 +1,23 @@
+import { Config, ConfigKey } from '../src/Config';
+
+describe('Conifg', () => {
+
+  it('should load secrets from environment variables.', () => {
+    const configFile = require('../json/config.json');
+    const secretValue = 'abc';
+    process.env.EXAMPLE_SECRET = secretValue;
+
+    const config = new Config(configFile);
+    const actualSecretValue = config[ConfigKey.ExampleSecret];
+
+    expect(actualSecretValue).toEqual(secretValue);
+  });
+
+  it('should throw error if secrets cannot be loaded from environment variables in production environment.', () => {
+    const configFile = require('../json/config.json');
+    configFile.devMode = false;
+    delete process.env.EXAMPLE_SECRET;
+
+    expect(() => { new Config(configFile) }).toThrowError();
+  });
+});
