021bee6002812c7c694f9edb73bae7dce84b1742
Henry Tsai <henrytsai@outlook.com>
2018-10-17 15:50:01 -0700

    Added Sidetree transaction read interface and implementation.

--- tests/mocks/MockBlockchain.ts
+++ tests/mocks/MockBlockchain.ts
@@ -1,6 +1,14 @@
+import Transaction from '../../src/Transaction';
 import { Blockchain } from '../../src/Blockchain';
 
 export default class MockBlockchain implements Blockchain {
   public async write (_anchorFileHash: string): Promise<void> {
+  }
+
+  public async read (_afterTransaction?: number): Promise<{ moreTransactions: boolean, transactions: Transaction[] }> {
+    return {
+      moreTransactions: false,
+      transactions: []
+    }
   }
 }
