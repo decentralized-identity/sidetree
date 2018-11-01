0289f813143051c21c1bb8197d4812119d62cd4b
Henry Tsai <henrytsai@outlook.com>
2018-10-31 17:02:56 -0700

    Fixed imports to enable Node.js resolve modules from generated .js files.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as getRawBody from 'raw-body';
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
-import RequestHandler from 'RequestHandler';
-import { toHttpStatus, Response } from 'Response';
+import RequestHandler from './RequestHandler';
+import { toHttpStatus, Response } from './Response';
 
 const requestHandler = new RequestHandler();
 const app = new Koa();
