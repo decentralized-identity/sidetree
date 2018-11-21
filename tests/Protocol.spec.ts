0bed32e9e5feb838ff01dc1f9039c304e98d9029
Henry Tsai <henrytsai@outlook.com>
2018-11-20 17:27:07 -0800

    Protocol - Generalized protocol 'starting block number' to 'starting blockchain time'.

--- tests/Protocol.spec.ts
+++ tests/Protocol.spec.ts
@@ -3,11 +3,11 @@ import { getProtocol, initializeProtocol } from '../src/Protocol';
 describe('Protocol', () => {
   initializeProtocol('protocol-test.json');
 
-  it('should fetch right protocol given the block number.', async () => {
+  it('should fetch right protocol given the logical blockchain time.', async () => {
     const protocol01 = getProtocol(1);
-    expect(protocol01.startingBlockNumber).toBe(0);
+    expect(protocol01.startingBlockchainTime).toBe(0);
 
     const protocol10 = getProtocol(500000);
-    expect(protocol10.startingBlockNumber).toBe(500000);
+    expect(protocol10.startingBlockchainTime).toBe(500000);
   });
 });
