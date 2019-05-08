eb697ffe3bbb533cbc71fcd16d970aa5d891a0fb
Martin Riedel <web@riedel-privat.de>
2019-05-08 15:58:38 -0500

    Allow replacement of config variables via ENV.

--- docker/bitcore-sidetree/bitcore-node-entrypoint.sh
+++ docker/bitcore-sidetree/bitcore-node-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i -- "s/\"testnet\"/\"${BITCOIN_NETWORK}\"/g" ./bitcore-node.json
+json -I -f bitcore-node.json -e "this.network=\"${BITCOIN_NETWORK}\""
+json -I -f ./node_modules/sidetree/config.json -e "this.privateKeyWIF=\"${BITCOIN_PRIVATE_KEY_WIF}\"; this.fees=\"${BITCOIN_FEE}\""
+
 ./node_modules/.bin/bitcore-node start
