b35e301881cd0f2bcaa8e0b497422dbbaa5ddf23
Logan Girvin <logirvin@microsoft.com>
2019-05-10 14:25:50 -0700

    Lint.

--- lib/bitcoin/BitcoinProcessor.ts
+++ lib/bitcoin/BitcoinProcessor.ts
@@ -64,7 +64,7 @@ export default class BitcoinProcessor {
    * generates a private key in WIF format
    * @param network Which bitcoin network to generate this key for
    */
-  public static generatePrivateKey(network: 'mainnet' | 'livenet' | 'testnet' | undefined): string {
+  public static generatePrivateKey (network: 'mainnet' | 'livenet' | 'testnet' | undefined): string {
     let bitcoreNetwork: Networks.Network | undefined;
     switch (network) {
       case 'mainnet':
