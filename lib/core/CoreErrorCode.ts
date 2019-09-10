f7678b8b8c1c75cd2b2af572a68219be207d0b27
Henry Tsai <henrytsai@outlook.com>
2019-09-09 18:04:27 -0700

    Blockchain client - Replaced generic error throwing with specific error codes.

---
+++ lib/core/CoreErrorCode.ts
@@ -0,0 +1,9 @@
+/**
+ * Error codes used by Sidetree core service.
+ */
+export default {
+  BlockchainGetLatestTimeResponseNotOk: 'blockchain_get_latest_time_response_not_ok',
+  BlockchainReadInvalidArguments: 'blockchain_read_invalid_arguments',
+  BlockchainReadResponseNotOk: 'blockchain_read_response_not_ok',
+  BlockchainWriteResponseNotOk: 'blockchain_write_response_not_ok'
+};
