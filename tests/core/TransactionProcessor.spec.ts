1b2b9c38fc951d0f4ea069f097f1d307014f04b2
Isaac Chen <ische@microsoft.com>
2020-01-02 13:36:28 -0800

    change per pr

--- tests/core/TransactionProcessor.spec.ts
+++ tests/core/TransactionProcessor.spec.ts
@@ -22,132 +22,133 @@ describe('TransactionProcessor', () => {
     transactionProcessor = new TransactionProcessor(downloadManager, operationStore);
   });
 
-  it('should ignore error and return true when AnchoredDataSerializer throws a sidetree error', async () => {
-    const anchoredData = 'Bad Format';
-    const mockTransaction: TransactionModel = {
-      transactionNumber: 1,
-      transactionTime: 1000000,
-      transactionTimeHash: '1000',
-      anchorString: anchoredData,
-      transactionFeePaid: 1,
-      normalizedTransactionFee: 1
-    };
-    const result = await transactionProcessor.processTransaction(mockTransaction);
-    expect(result).toBeTruthy();
-  });
-
-  it('should ignore error and return true when FeeManager throws a sidetree error', async () => {
-    const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 0 });
-    const mockTransaction: TransactionModel = {
-      transactionNumber: 1,
-      transactionTime: 1000000,
-      transactionTimeHash: '1000',
-      anchorString: anchoredData,
-      transactionFeePaid: 1,
-      normalizedTransactionFee: 1
-    };
-    const result = await transactionProcessor.processTransaction(mockTransaction);
-    expect(result).toBeTruthy();
-  });
-
-  it('should return true if anchor file hash is not valid', async () => {
-    spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
-      const result: FetchResult = { code: FetchResultCode.InvalidHash };
-      return new Promise((resolve) => {
-        resolve(result);
-      });
+  describe('prcoessTransaction', () => {
+    it('should ignore error and return true when AnchoredDataSerializer throws a sidetree error', async () => {
+      const anchoredData = 'Bad Format';
+      const mockTransaction: TransactionModel = {
+        transactionNumber: 1,
+        transactionTime: 1000000,
+        transactionTimeHash: '1000',
+        anchorString: anchoredData,
+        transactionFeePaid: 1,
+        normalizedTransactionFee: 1
+      };
+      const result = await transactionProcessor.processTransaction(mockTransaction);
+      expect(result).toBeTruthy();
     });
-    const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
-    const mockTransaction: TransactionModel = {
-      transactionNumber: 1,
-      transactionTime: 1000000,
-      transactionTimeHash: '1000',
-      anchorString: anchoredData,
-      transactionFeePaid: 999999,
-      normalizedTransactionFee: 1
-    };
-    const result = await transactionProcessor.processTransaction(mockTransaction);
-    expect(result).toBeTruthy();
-  });
 
-  it('should return true if fetch result code is max size exceeded', async () => {
-    spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
-      const result: FetchResult = { code: FetchResultCode.MaxSizeExceeded };
-      return new Promise((resolve) => {
-        resolve(result);
-      });
+    it('should ignore error and return true when FeeManager throws a sidetree error', async () => {
+      const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 0 });
+      const mockTransaction: TransactionModel = {
+        transactionNumber: 1,
+        transactionTime: 1000000,
+        transactionTimeHash: '1000',
+        anchorString: anchoredData,
+        transactionFeePaid: 1,
+        normalizedTransactionFee: 1
+      };
+      const result = await transactionProcessor.processTransaction(mockTransaction);
+      expect(result).toBeTruthy();
     });
-    const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
-    const mockTransaction: TransactionModel = {
-      transactionNumber: 1,
-      transactionTime: 1000000,
-      transactionTimeHash: '1000',
-      anchorString: anchoredData,
-      transactionFeePaid: 999999,
-      normalizedTransactionFee: 1
-    };
-    const result = await transactionProcessor.processTransaction(mockTransaction);
-    expect(result).toBeTruthy();
-  });
 
-  it('should return true if fetch result code is not a file', async () => {
-    spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
-      const result: FetchResult = { code: FetchResultCode.NotAFile };
-      return new Promise((resolve) => {
-        resolve(result);
+    it('should return true if anchor file hash is not valid', async () => {
+      spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
+        const result: FetchResult = { code: FetchResultCode.InvalidHash };
+        return new Promise((resolve) => {
+          resolve(result);
+        });
       });
+      const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
+      const mockTransaction: TransactionModel = {
+        transactionNumber: 1,
+        transactionTime: 1000000,
+        transactionTimeHash: '1000',
+        anchorString: anchoredData,
+        transactionFeePaid: 999999,
+        normalizedTransactionFee: 1
+      };
+      const result = await transactionProcessor.processTransaction(mockTransaction);
+      expect(result).toBeTruthy();
     });
-    const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
-    const mockTransaction: TransactionModel = {
-      transactionNumber: 1,
-      transactionTime: 1000000,
-      transactionTimeHash: '1000',
-      anchorString: anchoredData,
-      transactionFeePaid: 999999,
-      normalizedTransactionFee: 1
-    };
-    const result = await transactionProcessor.processTransaction(mockTransaction);
-    expect(result).toBeTruthy();
-  });
 
-  it('should return true if fetch result code is cas not reachable', async () => {
-    spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
-      const result: FetchResult = { code: FetchResultCode.CasNotReachable };
-      return new Promise((resolve) => {
-        resolve(result);
+    it('should return true if fetch result code is max size exceeded', async () => {
+      spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
+        const result: FetchResult = { code: FetchResultCode.MaxSizeExceeded };
+        return new Promise((resolve) => {
+          resolve(result);
+        });
       });
+      const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
+      const mockTransaction: TransactionModel = {
+        transactionNumber: 1,
+        transactionTime: 1000000,
+        transactionTimeHash: '1000',
+        anchorString: anchoredData,
+        transactionFeePaid: 999999,
+        normalizedTransactionFee: 1
+      };
+      const result = await transactionProcessor.processTransaction(mockTransaction);
+      expect(result).toBeTruthy();
     });
-    const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
-    const mockTransaction: TransactionModel = {
-      transactionNumber: 1,
-      transactionTime: 1000000,
-      transactionTimeHash: '1000',
-      anchorString: anchoredData,
-      transactionFeePaid: 999999,
-      normalizedTransactionFee: 1
-    };
-    const result = await transactionProcessor.processTransaction(mockTransaction);
-    expect(result).toBeFalsy();
-  });
 
-  it('should return true if fetch result code is not found', async () => {
-    spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
-      const result: FetchResult = { code: FetchResultCode.NotFound };
-      return new Promise((resolve) => {
-        resolve(result);
+    it('should return true if fetch result code is not a file', async () => {
+      spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
+        const result: FetchResult = { code: FetchResultCode.NotAFile };
+        return new Promise((resolve) => {
+          resolve(result);
+        });
       });
+      const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
+      const mockTransaction: TransactionModel = {
+        transactionNumber: 1,
+        transactionTime: 1000000,
+        transactionTimeHash: '1000',
+        anchorString: anchoredData,
+        transactionFeePaid: 999999,
+        normalizedTransactionFee: 1
+      };
+      const result = await transactionProcessor.processTransaction(mockTransaction);
+      expect(result).toBeTruthy();
     });
-    const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
-    const mockTransaction: TransactionModel = {
-      transactionNumber: 1,
-      transactionTime: 1000000,
-      transactionTimeHash: '1000',
-      anchorString: anchoredData,
-      transactionFeePaid: 999999,
-      normalizedTransactionFee: 1
-    };
-    const result = await transactionProcessor.processTransaction(mockTransaction);
-    expect(result).toBeFalsy();
-  });
 
+    it('should return true if fetch result code is cas not reachable', async () => {
+      spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
+        const result: FetchResult = { code: FetchResultCode.CasNotReachable };
+        return new Promise((resolve) => {
+          resolve(result);
+        });
+      });
+      const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
+      const mockTransaction: TransactionModel = {
+        transactionNumber: 1,
+        transactionTime: 1000000,
+        transactionTimeHash: '1000',
+        anchorString: anchoredData,
+        transactionFeePaid: 999999,
+        normalizedTransactionFee: 1
+      };
+      const result = await transactionProcessor.processTransaction(mockTransaction);
+      expect(result).toBeFalsy();
+    });
+
+    it('should return true if fetch result code is not found', async () => {
+      spyOn(downloadManager, 'download').and.callFake((): Promise<FetchResult> => {
+        const result: FetchResult = { code: FetchResultCode.NotFound };
+        return new Promise((resolve) => {
+          resolve(result);
+        });
+      });
+      const anchoredData = AnchoredDataSerializer.serialize({ anchorFileHash: '1stTransaction', numberOfOperations: 1 });
+      const mockTransaction: TransactionModel = {
+        transactionNumber: 1,
+        transactionTime: 1000000,
+        transactionTimeHash: '1000',
+        anchorString: anchoredData,
+        transactionFeePaid: 999999,
+        normalizedTransactionFee: 1
+      };
+      const result = await transactionProcessor.processTransaction(mockTransaction);
+      expect(result).toBeFalsy();
+    });
+  });
 });
