840636ae84ea3224749b606e5bf14525b09bce55
Isaac Chen <ische@microsoft.com>
2020-05-21 13:24:23 -0700

    move reveal value into signed data #679

--- tests/core/DeactivateOperation.spec.ts
+++ tests/core/DeactivateOperation.spec.ts
@@ -8,6 +8,21 @@ import SidetreeError from '../../lib/common/SidetreeError';
 
 describe('DeactivateOperation', async () => {
   describe('parse()', async () => {
+    it('should parse as expected', async (done) => {
+      const [, recoveryPrivateKey] = await Jwk.generateEs256kKeyPair();
+
+      const deactivateOperationRequest = await OperationGenerator.createDeactivateOperationRequest(
+        'unused-DID-unique-suffix',
+        'unused-recovery-reveal-value',
+        recoveryPrivateKey
+      );
+
+      const operationBuffer = Buffer.from(JSON.stringify(deactivateOperationRequest));
+      const result = await DeactivateOperation.parse(operationBuffer);
+      expect(result).toBeDefined();
+      done();
+    });
+
     it('should throw if operation contains unknown property', async (done) => {
       const [, recoveryPrivateKey] = await Jwk.generateEs256kKeyPair();
 
@@ -56,38 +71,6 @@ describe('DeactivateOperation', async () => {
         .parse(operationBuffer)).toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationMissingOrInvalidDidUniqueSuffix));
       done();
     });
-
-    it('should throw if recoveryRevealValue is not string.', async (done) => {
-      const [, recoveryPrivateKey] = await Jwk.generateEs256kKeyPair();
-
-      const deactivateOperationRequest = await OperationGenerator.createDeactivateOperationRequest(
-        'unused-DID-unique-suffix',
-        'unused-recovery-reveal-value',
-        recoveryPrivateKey
-      );
-
-      (deactivateOperationRequest.recovery_reveal_value as any) = 123; // Intentionally incorrect type.
-
-      const operationBuffer = Buffer.from(JSON.stringify(deactivateOperationRequest));
-      await expectAsync(DeactivateOperation.parse(operationBuffer))
-              .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationRecoveryRevealValueMissingOrInvalidType));
-      done();
-    });
-
-    it('should throw if recoveryRevealValue is too long.', async (done) => {
-      const [, recoveryPrivateKey] = await Jwk.generateEs256kKeyPair();
-
-      const deactivateOperationRequest = await OperationGenerator.createDeactivateOperationRequest(
-        'unused-DID-unique-suffix',
-        'super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal',
-        recoveryPrivateKey
-      );
-
-      const operationBuffer = Buffer.from(JSON.stringify(deactivateOperationRequest));
-      await expectAsync(DeactivateOperation.parse(operationBuffer))
-      .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationRecoveryRevealValueTooLong));
-      done();
-    });
   });
 
   describe('parseSignedDataPayload()', async () => {
@@ -95,13 +78,49 @@ describe('DeactivateOperation', async () => {
       const didUniqueSuffix = 'anyUnusedDidUniqueSuffix';
       const recoveryRevealValue = 'anyUnusedRecoveryRevealValue';
       const signedData = {
-        didUniqueSuffix,
-        recoveryRevealValue,
+        did_suffix: didUniqueSuffix,
+        recovery_reveal_value: recoveryRevealValue,
         extraProperty: 'An unknown extra property'
       };
       const encodedDelta = Encoder.encode(JSON.stringify(signedData));
-      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedDelta, didUniqueSuffix, recoveryRevealValue))
+      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedDelta, didUniqueSuffix))
         .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationSignedDataMissingOrUnknownProperty));
+      done();
+    });
+
+    it('should throw if signedData is missing expected properties.', async (done) => {
+      const didUniqueSuffix = 'anyUnusedDidUniqueSuffix';
+      const signedData = {};
+      const encodedDelta = Encoder.encode(JSON.stringify(signedData));
+      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedDelta, didUniqueSuffix))
+        .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationSignedDataMissingOrUnknownProperty));
+      done();
+    });
+
+    it('should throw if recoveryRevealValue is too long.', async (done) => {
+      const didUniqueSuffix = 'anyUnusedDidUniqueSuffix';
+      // tslint:disable-next-line
+      const recoveryRevealValue = 'super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal-super-long-reveal';
+      const signedData = {
+        did_suffix: didUniqueSuffix,
+        recovery_reveal_value: recoveryRevealValue
+      };
+      const encodedDelta = Encoder.encode(JSON.stringify(signedData));
+      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedDelta, didUniqueSuffix))
+        .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationRecoveryRevealValueTooLong));
+      done();
+    });
+
+    it('should throw if recoveryRevealValue is not a string.', async (done) => {
+      const didUniqueSuffix = 'anyUnusedDidUniqueSuffix';
+      const recoveryRevealValue = ['this is an array not a string'];
+      const signedData = {
+        did_suffix: didUniqueSuffix,
+        recovery_reveal_value: recoveryRevealValue
+      };
+      const encodedDelta = Encoder.encode(JSON.stringify(signedData));
+      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedDelta, didUniqueSuffix, recoveryRevealValue))
+        .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationRecoveryRevealValueMissingOrInvalidType));
       done();
     });
 
@@ -109,25 +128,12 @@ describe('DeactivateOperation', async () => {
       const didUniqueSuffix = 'anyUnusedDidUniqueSuffix';
       const recoveryRevealValue = 'anyUnusedRecoveryRevealValue';
       const signedData = {
-        didUniqueSuffix,
-        recoveryRevealValue
-      };
-      const encodedSignedData = Encoder.encode(JSON.stringify(signedData));
-      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedSignedData, 'mismatchingDidUniqueSuffix', recoveryRevealValue))
-        .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationSignedDidUniqueSuffixMismatch));
-      done();
-    });
-
-    it('should throw if signed `recovery_reveal_value` is mismatching.', async (done) => {
-      const didUniqueSuffix = 'anyUnusedDidUniqueSuffix';
-      const recoveryRevealValue = 'anyUnusedRecoveryRevealValue';
-      const signedData = {
         did_suffix: didUniqueSuffix,
         recovery_reveal_value: recoveryRevealValue
       };
       const encodedSignedData = Encoder.encode(JSON.stringify(signedData));
-      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedSignedData, didUniqueSuffix, 'mismatchingRecoveryRevealValue'))
-        .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationSignedRecoveryRevealValueMismatch));
+      await expectAsync((DeactivateOperation as any).parseSignedDataPayload(encodedSignedData, 'mismatchingDidUniqueSuffix', recoveryRevealValue))
+        .toBeRejectedWith(new SidetreeError(ErrorCode.DeactivateOperationSignedDidUniqueSuffixMismatch));
       done();
     });
   });
