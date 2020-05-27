0cb73be416a548edfc715b83af3200e831730a7f
Isaac Chen <ische@microsoft.com>
2020-05-26 17:24:25 -0700

    minor changes

--- tests/core/versions/test-version-1/VersionMetadata.ts
+++ tests/core/versions/test-version-1/VersionMetadata.ts
@@ -13,4 +13,4 @@ export default class VersionMetadata extends AbstractVersionMetadata {
     this.normalizedFeeToPerOperationFeeMultiplier = protocolParameters.normalizedFeeToPerOperationFeeMultiplier;
     this.hashAlgorithmInMultihashCode = protocolParameters.hashAlgorithmInMultihashCode;
   }
-};
+}
