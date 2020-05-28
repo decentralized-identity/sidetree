1f35fdf054dceec964876220b9bebab880e4cbb6
Henry Tsai <henrytsai@outlook.com>
2020-05-28 13:05:27 -0700

    Removed v0.7.0.

--- lib/core/versions/0.7.0/models/AnchoredData.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Defines model for the data which is to be anchored to the blockchain.
- */
-export default interface AnchoredData {
-  anchorFileHash: string;
-  numberOfOperations: number;
-}
