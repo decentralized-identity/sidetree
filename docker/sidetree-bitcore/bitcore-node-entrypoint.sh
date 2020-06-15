8e5b2265be8243e7929e0cbd3bc89ce7d04e3d24
Henry Tsai <henrytsai@outlook.com>
2020-06-15 16:52:08 -0700

    Upgraded IPFS lib to v0.46.0 + removed obsolete docker files.

--- docker/sidetree-bitcore/bitcore-node-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-json -I -f bitcore-node.json -e "this.network=\"${BITCOIN_NETWORK}\""
-json -I -f ./node_modules/sidetree/config.json -e "this.privateKeyWIF=\"${BITCOIN_PRIVATE_KEY_WIF}\"; this.fees=\"${BITCOIN_FEE}\""
-
-./node_modules/.bin/bitcore-node start
