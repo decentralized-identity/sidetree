8105b0ebb1041064efb7f0ef492d2da87b0094b7
Tobias <tobias.looker@mattr.global>
2020-07-09 11:32:37 +1200

    build: add conventional changelog support

---
+++ commitlint.config.js
@@ -0,0 +1,41 @@
+// Configuration is extended from @commitlint/config-conventional
+// @see https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional
+module.exports = {
+	rules: {
+		'body-leading-blank': [1, 'always'],
+		'body-max-line-length': [2, 'always', 100],
+		'footer-leading-blank': [1, 'always'],
+		'footer-max-line-length': [2, 'always', 100],
+		'header-max-length': [2, 'always', 100],
+		'scope-case': [2, 'always', 'lower-case'],
+		'subject-case': [
+			2,
+			'never',
+			['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+		],
+		"scope-empty": [2, "never"],
+        "scope-enum": [2, "always", [
+            "spec",
+            "ref-impl"
+        ]],
+		'type-case': [2, 'always', 'lower-case'],
+		'type-empty': [2, 'never'],
+		'type-enum': [
+			2,
+			'always',
+			[
+				'build',
+				'chore',
+				'ci',
+				'docs',
+				'feat',
+				'fix',
+				'perf',
+				'refactor',
+				'revert',
+				'style',
+				'test'
+			]
+		]
+	}
+};
