009edba500fd89bc37e83b89082841dc58ba6c2c
Henry Tsai <henrytsai@outlook.com>
2020-08-28 17:34:45 -0700

    fix(ref-imp): ninor comment update.

--- tests/generators/VegetaLoadGenerator.ts
+++ tests/generators/VegetaLoadGenerator.ts
@@ -12,9 +12,10 @@ export default class VegetaLoadGenerator {
   /**
    * Creates a Create request followed by an Update request for each DID.
    * Following targets files will be generated:
-   *   One targets file containing all Create requests;
+   *   One targets file containing all Create requests
    *   One targets file containing all Update requests
    *   One targets file containing all recovery requests
+    *  One targets file containing all deactivate requests
    * @param uniqueDidCount The number of unique DID to be generated.
    * @param endpointUrl The URL that the requests will be sent to.
    * @param absoluteFolderPath The folder that all the generated files will be saved to.
