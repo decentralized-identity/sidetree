67e1db188f22b26f864d9dc2d80571c8f364d4a0
Orie Steele <orie@transmute.industries>
2020-10-13 16:38:10 -0500

    chore(ref-imp): turn off fixture overwrite

--- tests/core/Resolver.spec.ts
+++ tests/core/Resolver.spec.ts
@@ -23,7 +23,7 @@ import RecoverOperation from '../../lib/core/versions/latest/RecoverOperation';
 import Resolver from '../../lib/core/Resolver';
 import { fixtureDriftHelper } from '../utils';
 
-const OVERWRITE_FIXTURES = true;
+const OVERWRITE_FIXTURES = false;
 
 describe('Resolver', () => {
   let resolver: Resolver;
