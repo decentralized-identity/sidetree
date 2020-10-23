6b99e5da7f5273749f2d66f3bb1c8d561b51da66
Henry Tsai <henrytsai@outlook.com>
2020-10-23 14:21:43 -0700

    fix(ref-imp): added a TODO to capture discussion needed

--- lib/core/versions/latest/PublicKeyPurpose.ts
+++ lib/core/versions/latest/PublicKeyPurpose.ts
@@ -2,6 +2,7 @@
  * DID Document public key purpose.
  */
 enum PublicKeyPurpose {
+  // TODO: #894 - Discuss `VerificationMethod` enum value in public key purpose.
   VerificationMethod = 'verificationMethod',
   Authentication = 'authentication',
   AssertionMethod = 'assertionMethod',
