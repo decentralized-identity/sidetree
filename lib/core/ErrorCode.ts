9ee807f47cf8e8a8d90e25946526af34295727b2
Henry Tsai <17891086+thehenrytsai@users.noreply.github.com>
2021-04-07 11:13:41 -0700

    feat(ref-imp): added error code metadata for batch writer loop failure event

--- lib/core/ErrorCode.ts
+++ lib/core/ErrorCode.ts
@@ -2,6 +2,7 @@
  * Error codes used by Sidetree core service.
  */
 export default {
+  BatchSchedulerWriteUnexpectedError: 'batch_scheduler_write_unexpected_error',
   BlockchainGetFeeResponseNotOk: 'blockchain_get_fee_response_not_ok',
   BlockchainGetLatestTimeResponseNotOk: 'blockchain_get_latest_time_response_not_ok',
   BlockchainGetLockResponseNotOk: 'blockchain_get_lock_response_not_ok',
@@ -9,7 +10,7 @@ export default {
   BlockchainReadInvalidArguments: 'blockchain_read_invalid_arguments',
   BlockchainReadResponseBodyNotJson: 'blockchain_read_response_body_not_json',
   BlockchainReadResponseNotOk: 'blockchain_read_response_not_ok',
-  BlockchainWriteResponseNotOk: 'blockchain_write_response_not_ok',
+  BlockchainWriteUnexpectedError: 'blockchain_write_unexpected_error',
   VersionManagerVersionStringNotFound: 'version_manager_version_string_not_found',
   VersionManagerVersionMetadataIncorrectType: 'version_manager_version_metadata_incorrect_type'
 };
